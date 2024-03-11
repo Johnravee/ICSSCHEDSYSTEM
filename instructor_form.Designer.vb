7e91b7ef063152535c003d958494c1c4ba53fef5
John Rave Mimay <johnravemimayn@gmail.com>
2024-03-11 16:17:00 +0800

    Delete unneccessary images

--- instructor_form.Designer.vb
+++ instructor_form.Designer.vb
@@ -26,20 +26,23 @@ Partial Class instructor_form
         Label1 = New Label()
         TextBox1 = New TextBox()
         Button1 = New Button()
-        PictureBox1 = New PictureBox()
         Label2 = New Label()
-        Button2 = New Button()
-        Button3 = New Button()
-        Button4 = New Button()
-        PictureBox2 = New PictureBox()
         Button5 = New Button()
         Button6 = New Button()
         Button7 = New Button()
         Button8 = New Button()
         Button9 = New Button()
         Button10 = New Button()
+        backBtn = New PictureBox()
+        PictureBox1 = New PictureBox()
+        PictureBox2 = New PictureBox()
+        minimize = New PictureBox()
+        closeBtn = New PictureBox()
+        CType(backBtn, ComponentModel.ISupportInitialize).BeginInit()
         CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
         CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        CType(minimize, ComponentModel.ISupportInitialize).BeginInit()
+        CType(closeBtn, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
         ' 
         ' Label1
@@ -47,111 +50,52 @@ Partial Class instructor_form
         Label1.AutoSize = True
         Label1.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
         Label1.ForeColor = Color.FromArgb(CByte(252), CByte(253), CByte(253))
-        Label1.Location = New Point(317, 188)
+        Label1.Location = New Point(198, 241)
+        Label1.Margin = New Padding(2, 0, 2, 0)
         Label1.Name = "Label1"
-        Label1.Size = New Size(89, 32)
+        Label1.Size = New Size(61, 21)
         Label1.TabIndex = 5
         Label1.Text = "Search"
         ' 
         ' TextBox1
         ' 
-        TextBox1.Location = New Point(405, 189)
-        TextBox1.Margin = New Padding(3, 2, 3, 2)
+        TextBox1.Location = New Point(260, 241)
+        TextBox1.Margin = New Padding(2, 1, 2, 1)
         TextBox1.Name = "TextBox1"
-        TextBox1.Size = New Size(432, 31)
+        TextBox1.Size = New Size(304, 23)
         TextBox1.TabIndex = 6
         ' 
         ' Button1
         ' 
         Button1.Image = CType(resources.GetObject("Button1.Image"), Image)
-        Button1.Location = New Point(840, 191)
-        Button1.Margin = New Padding(3, 2, 3, 2)
+        Button1.Location = New Point(564, 243)
+        Button1.Margin = New Padding(2, 1, 2, 1)
         Button1.Name = "Button1"
-        Button1.Size = New Size(38, 31)
+        Button1.Size = New Size(27, 19)
         Button1.TabIndex = 7
         Button1.UseVisualStyleBackColor = True
-        ' 
-        ' PictureBox1
-        ' 
-        PictureBox1.BackgroundImageLayout = ImageLayout.Stretch
-        PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), Image)
-        PictureBox1.Location = New Point(222, 38)
-        PictureBox1.Margin = New Padding(3, 2, 3, 2)
-        PictureBox1.Name = "PictureBox1"
-        PictureBox1.Size = New Size(185, 155)
-        PictureBox1.TabIndex = 8
-        PictureBox1.TabStop = False
         ' 
         ' Label2
         ' 
         Label2.AutoSize = True
         Label2.Font = New Font("Segoe UI", 16F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
         Label2.ForeColor = Color.FromArgb(CByte(252), CByte(253), CByte(253))
-        Label2.Location = New Point(475, 92)
+        Label2.Location = New Point(333, 183)
+        Label2.Margin = New Padding(2, 0, 2, 0)
         Label2.Name = "Label2"
-        Label2.Size = New Size(237, 45)
+        Label2.Size = New Size(161, 30)
         Label2.TabIndex = 9
         Label2.Text = "INSTRUCTORS"
-        ' 
-        ' Button2
-        ' 
-        Button2.BackColor = Color.FromArgb(CByte(0), CByte(81), CByte(61))
-        Button2.BackgroundImageLayout = ImageLayout.Stretch
-        Button2.Cursor = Cursors.Hand
-        Button2.FlatStyle = FlatStyle.Flat
-        Button2.ForeColor = Color.FromArgb(CByte(0), CByte(81), CByte(61))
-        Button2.Image = CType(resources.GetObject("Button2.Image"), Image)
-        Button2.Location = New Point(-21, -6)
-        Button2.Margin = New Padding(3, 2, 3, 2)
-        Button2.Name = "Button2"
-        Button2.Size = New Size(154, 110)
-        Button2.TabIndex = 12
-        Button2.UseVisualStyleBackColor = False
-        ' 
-        ' Button3
-        ' 
-        Button3.BackgroundImageLayout = ImageLayout.Stretch
-        Button3.Cursor = Cursors.Hand
-        Button3.FlatStyle = FlatStyle.Flat
-        Button3.ForeColor = Color.FromArgb(CByte(0), CByte(81), CByte(61))
-        Button3.Image = CType(resources.GetObject("Button3.Image"), Image)
-        Button3.Location = New Point(1027, 12)
-        Button3.Margin = New Padding(3, 2, 3, 2)
-        Button3.Name = "Button3"
-        Button3.Size = New Size(72, 34)
-        Button3.TabIndex = 13
-        Button3.UseVisualStyleBackColor = True
-        ' 
-        ' Button4
-        ' 
-        Button4.FlatStyle = FlatStyle.Flat
-        Button4.ForeColor = Color.FromArgb(CByte(0), CByte(81), CByte(61))
-        Button4.Image = CType(resources.GetObject("Button4.Image"), Image)
-        Button4.Location = New Point(1096, 12)
-        Button4.Margin = New Padding(3, 2, 3, 2)
-        Button4.Name = "Button4"
-        Button4.Size = New Size(55, 34)
-        Button4.TabIndex = 14
-        Button4.UseVisualStyleBackColor = True
-        ' 
-        ' PictureBox2
-        ' 
-        PictureBox2.BackgroundImageLayout = ImageLayout.None
-        PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), Image)
-        PictureBox2.Location = New Point(884, 38)
-        PictureBox2.Name = "PictureBox2"
-        PictureBox2.Size = New Size(166, 163)
-        PictureBox2.TabIndex = 15
-        PictureBox2.TabStop = False
         ' 
         ' Button5
         ' 
         Button5.BackColor = Color.White
         Button5.Font = New Font("Calibri", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         Button5.ForeColor = SystemColors.ActiveCaptionText
-        Button5.Location = New Point(57, 271)
+        Button5.Location = New Point(41, 291)
+        Button5.Margin = New Padding(2)
         Button5.Name = "Button5"
-        Button5.Size = New Size(336, 160)
+        Button5.Size = New Size(235, 96)
         Button5.TabIndex = 16
         Button5.Text = "Mr. Ponciano Delos Angeles"
         Button5.UseVisualStyleBackColor = False
@@ -161,9 +105,10 @@ Partial Class instructor_form
         Button6.BackColor = Color.White
         Button6.Font = New Font("Calibri", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         Button6.ForeColor = SystemColors.ActiveCaptionText
-        Button6.Location = New Point(412, 271)
+        Button6.Location = New Point(289, 291)
+        Button6.Margin = New Padding(2)
         Button6.Name = "Button6"
-        Button6.Size = New Size(336, 160)
+        Button6.Size = New Size(235, 96)
         Button6.TabIndex = 17
         Button6.Text = "Mr. Ponciano Delos Angeles"
         Button6.UseVisualStyleBackColor = False
@@ -173,9 +118,10 @@ Partial Class instructor_form
         Button7.BackColor = Color.White
         Button7.Font = New Font("Calibri", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         Button7.ForeColor = SystemColors.ActiveCaptionText
-        Button7.Location = New Point(769, 271)
+        Button7.Location = New Point(539, 291)
+        Button7.Margin = New Padding(2)
         Button7.Name = "Button7"
-        Button7.Size = New Size(336, 160)
+        Button7.Size = New Size(235, 96)
         Button7.TabIndex = 18
         Button7.Text = "Mr. Ponciano Delos Angeles"
         Button7.UseVisualStyleBackColor = False
@@ -185,9 +131,10 @@ Partial Class instructor_form
         Button8.BackColor = Color.White
         Button8.Font = New Font("Calibri", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         Button8.ForeColor = SystemColors.ActiveCaptionText
-        Button8.Location = New Point(769, 455)
+        Button8.Location = New Point(539, 401)
+        Button8.Margin = New Padding(2)
         Button8.Name = "Button8"
-        Button8.Size = New Size(336, 160)
+        Button8.Size = New Size(235, 96)
         Button8.TabIndex = 21
         Button8.Text = "Mr. Ponciano Delos Angeles"
         Button8.UseVisualStyleBackColor = False
@@ -197,9 +144,10 @@ Partial Class instructor_form
         Button9.BackColor = Color.White
         Button9.Font = New Font("Calibri", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         Button9.ForeColor = SystemColors.ActiveCaptionText
-        Button9.Location = New Point(412, 455)
+        Button9.Location = New Point(289, 401)
+        Button9.Margin = New Padding(2)
         Button9.Name = "Button9"
-        Button9.Size = New Size(336, 160)
+        Button9.Size = New Size(235, 96)
         Button9.TabIndex = 20
         Button9.Text = "Mr. Ponciano Delos Angeles"
         Button9.UseVisualStyleBackColor = False
@@ -209,59 +157,120 @@ Partial Class instructor_form
         Button10.BackColor = Color.White
         Button10.Font = New Font("Calibri", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         Button10.ForeColor = SystemColors.ActiveCaptionText
-        Button10.Location = New Point(57, 455)
+        Button10.Location = New Point(41, 401)
+        Button10.Margin = New Padding(2)
         Button10.Name = "Button10"
-        Button10.Size = New Size(336, 160)
+        Button10.Size = New Size(235, 96)
         Button10.TabIndex = 19
         Button10.Text = "Mr. Ponciano Delos Angeles"
         Button10.UseVisualStyleBackColor = False
         ' 
+        ' backBtn
+        ' 
+        backBtn.BackColor = Color.Transparent
+        backBtn.Cursor = Cursors.Hand
+        backBtn.Image = My.Resources.Resources.backBTN_removebg_preview1
+        backBtn.Location = New Point(12, 12)
+        backBtn.Name = "backBtn"
+        backBtn.Size = New Size(54, 50)
+        backBtn.SizeMode = PictureBoxSizeMode.Zoom
+        backBtn.TabIndex = 22
+        backBtn.TabStop = False
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.BackgroundImageLayout = ImageLayout.Zoom
+        PictureBox1.Image = My.Resources.Resources.gagecdm1
+        PictureBox1.Location = New Point(140, 68)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(102, 105)
+        PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox1.TabIndex = 23
+        PictureBox1.TabStop = False
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), Image)
+        PictureBox2.Location = New Point(600, 68)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(100, 105)
+        PictureBox2.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox2.TabIndex = 24
+        PictureBox2.TabStop = False
+        ' 
+        ' minimize
+        ' 
+        minimize.BackColor = Color.Transparent
+        minimize.Cursor = Cursors.Hand
+        minimize.Image = My.Resources.Resources.minimize_removebg_preview
+        minimize.Location = New Point(760, 1)
+        minimize.Name = "minimize"
+        minimize.Size = New Size(23, 25)
+        minimize.SizeMode = PictureBoxSizeMode.Zoom
+        minimize.TabIndex = 26
+        minimize.TabStop = False
+        ' 
+        ' closeBtn
+        ' 
+        closeBtn.BackColor = Color.Transparent
+        closeBtn.Cursor = Cursors.Hand
+        closeBtn.Image = My.Resources.Resources.xButton_removebg_preview1
+        closeBtn.Location = New Point(789, 1)
+        closeBtn.Name = "closeBtn"
+        closeBtn.Size = New Size(23, 25)
+        closeBtn.SizeMode = PictureBoxSizeMode.Zoom
+        closeBtn.TabIndex = 25
+        closeBtn.TabStop = False
+        ' 
         ' instructor_form
         ' 
-        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         AutoSize = True
         BackColor = Color.FromArgb(CByte(0), CByte(81), CByte(61))
         BackgroundImageLayout = ImageLayout.Zoom
-        ClientSize = New Size(1163, 665)
+        ClientSize = New Size(814, 572)
+        Controls.Add(minimize)
+        Controls.Add(closeBtn)
+        Controls.Add(PictureBox2)
+        Controls.Add(PictureBox1)
+        Controls.Add(backBtn)
         Controls.Add(Button8)
         Controls.Add(Button9)
         Controls.Add(Button10)
         Controls.Add(Button7)
         Controls.Add(Button6)
         Controls.Add(Button5)
-        Controls.Add(PictureBox2)
-        Controls.Add(Button4)
-        Controls.Add(Button3)
-        Controls.Add(Button2)
         Controls.Add(Label2)
-        Controls.Add(PictureBox1)
         Controls.Add(Button1)
         Controls.Add(TextBox1)
         Controls.Add(Label1)
         ForeColor = SystemColors.ActiveBorder
         FormBorderStyle = FormBorderStyle.None
-        Margin = New Padding(3, 2, 3, 2)
+        Margin = New Padding(2, 1, 2, 1)
         Name = "instructor_form"
         Text = "Instructor"
+        CType(backBtn, ComponentModel.ISupportInitialize).EndInit()
         CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
         CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        CType(minimize, ComponentModel.ISupportInitialize).EndInit()
+        CType(closeBtn, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
         PerformLayout()
     End Sub
     Friend WithEvents Label1 As Label
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents Button1 As Button
-    Friend WithEvents PictureBox1 As PictureBox
     Friend WithEvents Label2 As Label
-    Friend WithEvents Button2 As Button
-    Friend WithEvents Button3 As Button
-    Friend WithEvents Button4 As Button
-    Friend WithEvents PictureBox2 As PictureBox
     Friend WithEvents Button5 As Button
     Friend WithEvents Button6 As Button
     Friend WithEvents Button7 As Button
     Friend WithEvents Button8 As Button
     Friend WithEvents Button9 As Button
     Friend WithEvents Button10 As Button
+    Friend WithEvents backBtn As PictureBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents minimize As PictureBox
+    Friend WithEvents closeBtn As PictureBox
 End Class
--- instructor_form.vb
+++ instructor_form.vb
@@ -1,22 +1,3 @@
 ﻿Public Class instructor_form
 
-    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
-
-    End Sub
-
-    Private Sub Button2_Click(sender As Object, e As EventArgs)
-
-    End Sub
-
-    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
-
-    End Sub
-
-    Private Sub Panel2_Paint(sender As Object, e As PaintEventArgs)
-
-    End Sub
-
-    Private Sub instructor_form_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
 End Class
